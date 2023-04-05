c7a351380f868a512eb24e8916c05dae0d84a99d
Jake Sweger <jsweg13@gmail.com>
2023-04-05 11:50:28 -0400

    Create getComputerChoice function

---
+++ game.js
@@ -0,0 +1,17 @@
+//function getComputerChoice(){
+    let rps = '';
+    let randomNum = parseInt(Math.floor(Math.random()*3));
+    console.log(randomNum);
+    console.log(rps);
+
+    if (randomNum = 0){
+        rps = "Rock";
+    } else if (randomNum = 1){
+        rps = "Paper"
+    }else if (randomNum = 2){
+        rps = "Scissors"
+    }
+    console.log(rps);
+
+    //return rps;
+//}
