d181e57e3d2fce144770c56b5df51813792dfb62
Jake Sweger <jsweg13@gmail.com>
2023-04-05 15:01:41 -0400

    Add else statement for winning/losing conditin

--- game.js
+++ game.js
@@ -1,26 +1,44 @@
-let playerSelection = prompt("Enter Rock, Paper, or Scissors:").toUpperCase();
+function playerSelection(){
+    let playerSelection = prompt("Enter Rock, Paper, or Scissors:").toUpperCase();
+    return playerSelection;
+}
 
-if (playerSelection == "ROCK" || playerSelection == "PAPER" || playerSelection == "SCISSORS"){
+/*if (playerSelection == "ROCK" || playerSelection == "PAPER" || playerSelection == "SCISSORS"){
     console.log(playerSelection);
 } else {
     playerSelection = prompt("You must enter Rock, Paper, or Scissors:").toUpperCase();
 }
+
+/*while (pl < 5) {
+    text += i + "<br>";
+    i++;
+  }*/
 
 function getComputerChoice(){
     let rps = '';
     let randomNum = parseInt(Math.floor(Math.random()*3));
 
     if (randomNum == 0){
-        rps = "Rock";
+        rps = "ROCK";
     } else if (randomNum == 1){
-        rps = "Paper"
+        rps = "PAPER"
     }else if (randomNum == 2){
-        rps = "Scissors"
+        rps = "SCISSORS"
     }
-
     return rps;
 }
 
+let cpu = getComputerChoice();
+let player = playerSelection();
 
+if (player == cpu){
+    alert("Tie");
+} else if ((player == "ROCK" && cpu == "SCISSORS") || (player == "SCISSORS" && cpu == "PAPER") || (player == "PAPER" && cpu == "ROCK")){
+    alert("Player Wins");
+} else {
+    let message = ""
+    return"CPU Wins")
+}
 
-console.log(getComputerChoice());
+console.log(player);
+console.log(cpu);
